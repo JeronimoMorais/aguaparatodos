55c26eb6fb874429a6847070658bbc4f13d9c77e
Bruno Souza <souza.brunha@gmail.com>
2025-07-17 19:38:23 -0300

    refactor: redimensionamento dos botoes e logo na tela de login

--- app/src/main/java/com/example/aguaparatodos/LoginActivity.kt
+++ app/src/main/java/com/example/aguaparatodos/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.aguaparatodos
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,6 +11,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -56,28 +61,43 @@ fun LoginPage(modifier: Modifier = Modifier) {
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "Logo do aplicativo",
             modifier = Modifier
-                .size(200.dp)
+                .size(160.dp)
                 .padding(top = 16.dp),
             contentScale = ContentScale.Fit
         )
 
         Column(
             modifier = Modifier
-                .padding(top = 24.dp),
+                .padding(top = 24.dp)
+                .widthIn(max = 256.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalAlignment = CenterHorizontally
         ) {
-            Button(
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                    Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+                    Toast.makeText(activity, "Usuário autenticado!", Toast.LENGTH_LONG).show()
+                    activity?.startActivity(
+                        Intent(activity, MainActivity::class.java).setFlags(
+                            FLAG_ACTIVITY_SINGLE_TOP
+                        )
+                    )
+
                 }
             ) {
+                Icon(Icons.AutoMirrored.Default.Send, contentDescription = "Google Icon")
+                Spacer(modifier = Modifier.width(8.dp))
                 Text("Login com Google")
             }
             Button(
+                modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                    email = ""
-                    password = ""
+                    Toast.makeText(activity, "Usuário anônimo!", Toast.LENGTH_LONG).show()
+                    activity?.startActivity(
+                        Intent(activity, MainActivity::class.java).setFlags(
+                            FLAG_ACTIVITY_SINGLE_TOP
+                        )
+                    )
                 }
             ) {
                 Text("Entrar anonimamente")
